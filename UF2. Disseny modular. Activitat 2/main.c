6dc81e903f7f7960c855a539c8e4a624bdd1c751
abohera <77778501+abohera@users.noreply.github.com>
2022-02-02 14:04:31 +0100

    Update main.c

--- UF2. Disseny modular. Activitat 2/main.c
+++ UF2. Disseny modular. Activitat 2/main.c
@@ -5,7 +5,7 @@ void main(){
     int segundos = 0;
     printf("Introduce los segundos: ");
     scanf("%d", &segundos);
-    if (segundos <= 0){
+    if (segundos < 0){
         printf("\nError, Debe indicar un número positivo");
         printf("\nIntroduzca los segundos de nuevo: ");
         scanf("%d", &segundos);

4726bcd9a55d62c655a25c877431b28eb2315a0d
abohera <77778501+abohera@users.noreply.github.com>
2022-02-02 13:53:51 +0100

    Update functions.c

--- UF2. Disseny modular. Activitat 1/functions.c
+++ UF2. Disseny modular. Activitat 1/functions.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int validation(int num){
-    for (int i = 0; i < 3; i++){
-        if (num < 10 | num > 5000){
+    for (int i = 0; i < TIMES; i++){
+        if (num < MIN | num > MAX){
             printf("Error, Vuelve a introducir un numero entre 10 y 5000 incluidos: ");
             scanf("%d", &num);
         }
